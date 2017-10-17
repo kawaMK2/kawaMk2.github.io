b47ffe42db4f6cdd3153904315f6561181be27d0
kawaMK2 <shintarooo0079@gmail.com>
2017-10-18 01:22:21 +0900

    add gallery page

---
+++ js/gallery.js
@@ -0,0 +1,14 @@
+$(function () {
+    // nested
+    // $('#container').nested({
+    //     minWidth: 50,
+    //     resizeToFit: true,
+    //     resizeToFitOptions: {
+    //         resizeAny: true // 空白より大きいまたは小さなボックスを空白に合うまでリサイズする
+    //     }
+    //
+    // });
+
+});
+
+baguetteBox.run('.tz-gallery');
